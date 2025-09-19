fd971a9e7e961a1f8ea4e9f6f8011b27bc497fb6
muges <muges@LAPTOP-O4K0FNH1>
2025-09-19 11:32:34 +0530

    Updated

---
+++ src/main/java/selenium/selenium.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selenium {
+
+	public static void main(String[] args) {
+		
+		
+			ChromeDriver driver=new ChromeDriver();
+			driver.get("http://leaftaps.com/opentaps/control/main");
+			}
+
+	}
+
